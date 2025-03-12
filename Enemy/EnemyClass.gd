81e53d049c1a43c7688030d9cd024ead91676bac
RileyWong26 <rileywongwong@gmail.com>
2025-03-12 18:55:42 -0400

    enemy stuff

--- Enemy/EnemyClass.gd
+++ Enemy/EnemyClass.gd
@@ -6,11 +6,11 @@ extends Area2D
 @export var HEALTH := 10.0
 
 func _process(delta: float) -> void:
-	var player := get_tree().current_scene.find_child("Player", true, false)
-	var direction = (player.global_position - global_position).normalized()
-	
-	global_position += direction * SPEED * delta
-
+	#var player := get_tree().current_scene.find_child("Player", true, false)
+	#var direction = (player.global_position - global_position).normalized()
+	#
+	#global_position += direction * SPEED * delta
+	pass
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
