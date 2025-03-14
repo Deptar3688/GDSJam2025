ad3bd537acd2d22b36dd85fdb999bcc5013c7860
Deptar3688 <davidlin3688@gmail.com>
2025-03-14 17:04:40 -0400

    added shooting

---
+++ Bullets/player_bullet.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+var SPEED:= 300
+var direction: Vector2
+
+func _process(delta):
+	global_position += direction*SPEED*delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
---
+++ scenes/main.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var player:= $Player 
+@onready var player_bullet_node:= $PlayerBullets
+@export var player_bullet_tscn: PackedScene
+@onready var shoot_cooldown:= $Player/ShootCD
+
+func _ready():
+	pass # Replace with function body.
+	pass
+
+
+func _process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and shoot_cooldown.is_stopped():
+		var player_bullet = player_bullet_tscn.instantiate()
+		player_bullet.global_position = player.global_position
+		player_bullet.direction = (get_global_mouse_position() - player.global_position).normalized()
+		player_bullet_node.add_child(player_bullet)
+		shoot_cooldown.start()
+	
